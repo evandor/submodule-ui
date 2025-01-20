0cb89d27a4ae342324c5eeea7a8c930164b170bc
Carsten Gräf <evandor@gmail.com>
2025-01-20 11:03:57 +0100

    working on sessions

--- stores/uiStore.ts
+++ stores/uiStore.ts
@@ -12,6 +12,7 @@ import { useRoute, useRouter } from 'vue-router'
 export enum DrawerTabs {
   BOOKMARKS = 'bookmarks',
   OPEN_TABS = 'openTabs',
+  SESSIONS = 'sessions',
   UNASSIGNED_TABS = 'unassignedTabs',
   GROUP_BY_HOST_TABS = 'groupedByHostTabs',
   SAVED_TABS = 'savedTabs',
